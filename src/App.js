a9703d441b407efb4a12644fc9ceabd903c4b353
JonathanDeCastro <jonathan.f.p.de.castro@gmail.com>
2023-03-31 11:25:29 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+// Array com 2 elementos
+// 1 - listaDeTarefas
+// 2 - função para declarar valor da lista de tarefas
+let [lista, setLista] = useState([]);
+let [novoItem, setNovoItem] = useState("");
+
+useEffect(() => {
+  setLista(["Tarefa1", "Tarefa2", "Tarefa3", "Tarefa4"]);
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='new-item'>
+        <input placeholder="Tarefa" value={novoItem} onChange={value => setNovoItem(value.target.value)} type="text" />
+        <button onClick={() => adicionarNovoItem()}>Adicionar</button>
+      </div>
+      <ul className='todo-list'>
+        {lista.map((item, index) => (
+          <li key={index} className='todo-item'>
+            {item}
+            <button onClick={() => deletarItem(index)}>Deletar</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
+
+    function adicionarNovoItem() {
+      if (novoItem.length <= 0) {
+        alert("Por favor, digite algo no campo 'Tarefa'")
+        return;
+      }
+
+      let itemIndex = lista.indexOf(novoItem);
+      if (itemIndex >= 0) {
+        alert("Você já adicionou esta tarefa!");
+        return;
+      }
+
+      setLista([...lista, novoItem]);
+      setNovoItem("");
+    };
+
+    function deletarItem(index) {
+      let tmpArray = [...lista];
+      tmpArray.splice(index, 1)
+
+      setLista(tmpArray);
+    }
+
 }
 
 export default App;
